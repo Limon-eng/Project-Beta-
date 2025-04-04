aa543063ac86a13a4ea9ba078328c9ceaff731ff
Limon-eng <limonbapari6@gmail.com>
2025-04-04 17:29:12 +0600

    noti.js

--- scripts/cmds/noti.js
+++ scripts/cmds/noti.js
@@ -31,7 +31,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "Bby ki ki pathaibo oita bolo 🐸",
-			notification: "~🗣️ NOTIFICATION FROM ADMIN Ew'r ShAn's",
+			notification: "~🗣️ NOTIFICATION FROM ADMIN LÏMØÑ",
 			sendingNotification: "Start sending notification from admin bot to %1 chat groups",
 			sentNotification: "✅ Sent notification to %1 groups successfully",
 			errorSendingNotification: "An error occurred while sending to %1 groups:\n%2"
